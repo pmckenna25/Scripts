0db302c2f138188a3b16bb4d909db009cf9ea75b
Philip Mc Kenna <44911296+pmckenna25@users.noreply.github.com>
2019-03-24 13:50:35 +0000

    Add files via upload

---
+++ awscli-kops.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+apt-get install awscli -y
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops-linux-amd64
+mv kops-linux-amd64 /usr/local/bin/kops
