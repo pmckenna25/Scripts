a4397a3303520280ed791dd5feb64c42172114f5
Philip Mc Kenna <44911296+pmckenna25@users.noreply.github.com>
2019-03-27 20:51:40 +0000

    Script changed to not include php installs

--- fromS3bucketToWebServerEC2.bash
+++ fromS3bucketToWebServerEC2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-apt-get install apache2 php libapache2-mod-php awscli -y
+apt-get install apache2 awscli -y
 rm -rf /var/www/html/*
-aws s3 cp --recursive s3://bvdevphotobucket/ /var/www/html
+aws s3 cp --recursive s3://week-10-assignment/ /var/www/html
