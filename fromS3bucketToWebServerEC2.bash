51b5e4fd0e1a1609c994744025ed750e1ef12494
Philip Mc Kenna <44911296+pmckenna25@users.noreply.github.com>
2019-03-27 22:03:43 +0000

    line two updated

--- fromS3bucketToWebServerEC2.bash
+++ fromS3bucketToWebServerEC2.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 apt-get install apache2 awscli -y
 rm -rf /var/www/html/*
 aws s3 cp --recursive s3://week-10-assignment/ /var/www/html
